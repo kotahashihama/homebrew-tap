97afcfdd2a752eeef0b157cc14994adeb37f9f57
Kota Hashihama <kotahashihama@gmail.com>
2024-11-27 13:36:11 +0900

    first commit

---
+++ Formula/pceamless.rb
@@ -0,0 +1,11 @@
+class Pceamless < Formula
+  desc "pceamless は macOS の作業環境を別のPCに移行するためのツールです。"
+  homepage ""
+  url "https://github.com/kotahashihama/pceamless/releases/download/0.0.1/pceamless"
+  sha256 "a2bb750f68e7c3ffe1b646c99cd4ca525ea28e4cab9b3aa9e96ddf48c0aaff4f"
+  license ""
+
+  def install
+    bin.install "pceamless"
+  end
+end
